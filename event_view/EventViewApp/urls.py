350b9535410a5357cb9392f018bbe3c648564e62
aki <akisgithub@gmail.com>
2020-09-24 03:28:32 +0900

    add: イベント編集用のページを追加

--- event_view/EventViewApp/urls.py
+++ event_view/EventViewApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.event_view, name='event_view'),
-    path('edit', views.event_edit, name='event_edit')
+    path('<int:event_id>/edit', views.event_edit, name='event_edit'),
+    path('<int:event_id>/save', views.event_edit, name='event_save')
 ]
--- event_view/EventViewApp/views.py
+++ event_view/EventViewApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.forms.models import model_to_dict
 
 from .models import Event
+from .forms import EventForm
 
 
 def event_view(request):
@@ -12,5 +14,27 @@ def event_view(request):
     return render(request, 'EventViewApp/event_view.html', context)
 
 
-def event_edit(request):
-    return HttpResponse("event edit page")
+
+def event_edit(request, event_id):
+    try:
+        event = Event.objects.get(pk=event_id)
+    except Event.DoesNotExist:
+        raise Http404("Event does not exist")
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save(commit=True)
+        else:
+            print("ERROR FORM INVALID")
+            for elm in form:
+                print(elm)
+    context = {
+        'event': event
+        # 'title': event.title,
+        # 'start_date': event.start_date,
+        # 'end_date': event.end_date,
+        # 'content': event.content,
+        # 'period': event.period,
+        # 'url': event.url
+    }
+    return render(request, 'EventViewApp/edit.html', context)
