3718b2e7a6f4250fa5fe2dc3e17d2d38e66ec996
aki <akisgithub@gmail.com>
2020-09-30 15:20:11 +0900

    add: 期間ボタンを押したら、その期間のイベントのみが表示されるように

--- event_view/static/js/event_view.js
+++ event_view/static/js/event_view.js
@@ -1,9 +1,16 @@
-// 期間フィルタボタンが押されたらactiveになるように
+// 期間フィルタボタンが押されたときの処理
 $('input[name="period_type"]').on("change", function () {
   label = $('label[for="' + this.id + '"]');
+  posters = $('div[class="poster"][name="' + this.value + '"]');
   if ($(this).prop("checked")) {
     label.addClass("active");
+    posters.each(function (i, elm) {
+      $(elm).parent().show("fast");
+    });
   } else {
     label.removeClass("active");
+    posters.each(function (i, elm) {
+      $(elm).parent().hide("fast");
+    });
   }
 });
