8909792f73a96d5afd98d52f23582eec1cd068bb
aki <akisgithub@gmail.com>
2020-10-30 02:09:28 +0900

    ref: コメント追加

--- event_view/static/js/event_view.js
+++ event_view/static/js/event_view.js
@@ -1,7 +1,10 @@
 // 期間フィルタボタンが押されたときの処理
 $('input[name="period_type"]').on("change", function () {
+  // ボタンに対応するラベルを取得
   label = $('label[for="' + this.id + '"]');
+  // 各イベントの poster を取得
   posters = $('div[class="poster"][name="' + this.value + '"]');
+  // ラジオボタンがチェックされていればボタンをactiveにして、ポスターを表示させる
   if ($(this).prop("checked")) {
     label.addClass("active");
     posters.each(function (i, elm) {
