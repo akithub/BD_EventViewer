fd3f024220c0373777982101eeeaf1cf25450458
aki <akisgithub@gmail.com>
2020-11-14 16:05:08 +0900

    add: アップデート情報告知モーダルのUI実装

--- event_view/static/js/event_view.js
+++ event_view/static/js/event_view.js
@@ -17,3 +17,30 @@ $('input[name="period_type"]').on("change", function () {
     });
   }
 });
+// modal
+// modal open
+$("#info_btn").on("click", function () {
+  $("#updateInfoModal").modal("show");
+});
+// テキストエリアの値をmarkdown表示にする
+let updatePreview = function (elm) {
+  if (!$(elm).val()) {
+    return;
+  }
+  let html = marked($(elm).val());
+  $("#update-info").html(html);
+};
+// キー入力がされた際にmarkdown表示を更新
+$(function () {
+  $("#markdown_edit").keyup(function () {
+    updatePreview(this);
+  });
+  updatePreview($("#markdwon_edit"));
+});
+// info編集エディタ
+// 初期は非表示に
+$("#update-info-editor").hide();
+//表示切り替え
+$("#editor-toggle-btn").on("click", function () {
+  $("#update-info-editor").toggle();
+});
